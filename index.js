0793a311b226e5ec5b999cd68c04ff7cd68176fe
Mateusz M Trybunia <Mateusz.m.trybunia@gmail.com>
2023-09-13 11:54:27 -0500

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(drivers, search) {
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase() === search.toLowerCase()
+        }
+    )
+}
+
+function fuzzyMatch(drivers, search) {
+    return drivers.filter(function(driver) {
+        return driver.toUpperCase().substring([0], search.length) === search.toUpperCase()
+        }
+    )
+}
+
+function matchName(drivers, search) {
+    return drivers.filter(function(driver) {
+        return driver.name === search
+        }
+    )
+}
